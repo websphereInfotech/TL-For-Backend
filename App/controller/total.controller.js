dbeb34b1b1cbd48556d8e89b9d4b0674decd8ad4
skilljeminkikani <skilljeminkikani9510@gmail.com>
2023-10-14 13:41:38 +0530

    changes

--- App/controller/total.controller.js
+++ App/controller/total.controller.js
@@ -1,8 +1,10 @@
 const Total = require("../model/total.model");
+const user = require("../model/user.model");
 
 exports.totalCreate = async (req, res) => {
   try {
-    const { user_id, description, area, size, rate, quantity, total } = req.body;
+    const { user_id, description, area, size, rate, quantity, total } =
+      req.body;
 
     const totalOfAll = await Total.create({
       user_id,
@@ -19,6 +21,47 @@ exports.totalCreate = async (req, res) => {
       status: "Success",
       message: "Total Create Successfully",
       data: totalOfAll,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+// updateTotal
+exports.totalupdate = async (req, res) => {
+  try {
+    const { description, area, size, rate, quantity, total } = req.body;
+    const userId = req.params.id;
+    console.log(userId);
+    const updatedTotal = await Total.findOneAndUpdate(
+      { user_id: userId },
+      {
+        $set: {
+          description,
+          area,
+          size,
+          rate,
+          quantity,
+          total,
+        },
+      },
+      { new: true }
+    );
+
+    if (!updatedTotal) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Total not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      message: "Total updated successfully",
+      data: updatedTotal,
     });
   } catch (error) {
     res.status(404).json({
--- App/router/total.Routes.js
+++ App/router/total.Routes.js
@@ -1,8 +1,10 @@
 var express = require("express")
 var router = express.Router();
-const { totalCreate } = require('../controller/total.controller')
+const { totalCreate, totalupdate } = require('../controller/total.controller')
 const {verifytoken}=require('../middlware/auth')
 
 router.post("/total/create", verifytoken, totalCreate);
+
+router.put("/total/update/:id", verifytoken, totalupdate);
 
 module.exports = router
