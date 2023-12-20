9801c1b69e8cfd7506cab4b50da7df98904a8dac
vruti vadsak <124115737+vruti20@users.noreply.github.com>
2023-12-20 12:38:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,11 +27,7 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+app.use(cors);
 connectDB();
 
 app.use("/api", loginRoute);
