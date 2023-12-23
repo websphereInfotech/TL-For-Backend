4b30842d420f5afd4147b5b3e05fcf622f7e6981
ShwetaBeladiya <shwetabeladiya0504@gmail.com>
2023-12-23 12:39:41 +0530

    change

--- app.js
+++ app.js
@@ -40,14 +40,6 @@ app.use("/api", followRoutes);
 app.use("/api", totalRoutes);
 app.use("/api", viewRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(port, () => {
   console.log(`Server is running to port ${port}`);
 });
