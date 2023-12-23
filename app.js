37080b849f093338f4e18396bbfc451af78e9c47
ShwetaBeladiya <shwetabeladiya0504@gmail.com>
2023-12-23 15:24:20 +0530

    change

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use("/api", viewRoutes);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () => {
