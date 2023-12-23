e0b79211e951ba3a2a6f91bd384d1066a5545516
ShwetaBeladiya <shwetabeladiya0504@gmail.com>
2023-12-23 14:30:20 +0530

    app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use("/api", totalRoutes);
 app.use("/api", viewRoutes);
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder
-  app.use('/', express.static(join(__dirname, 'client', 'build')));
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
   
   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
