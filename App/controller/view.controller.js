2e0ac5653e7b4c0b22d97adbe374d39e6e01819c
ShwetaBeladiya <shwetabeladiya0504@gmail.com>
2023-12-29 13:51:13 +0530

    api

--- App/controller/view.controller.js
+++ App/controller/view.controller.js
@@ -39,7 +39,8 @@ exports.AllFiles = async (req, res) => {
       { users, Totalwithuser, status }
     );
     console.log("html", html);
-        const executablePath ='C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+        // const executablePath ='C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+        const executablePath ='C:\\Users\\Admin\\.cache\\puppeteer\\chrome\\win64-119.0.6045.105\\chrome-win64\\chrome.exe'
     const browser = await puppeteer.launch({
       executablePath,
       headless:false,
